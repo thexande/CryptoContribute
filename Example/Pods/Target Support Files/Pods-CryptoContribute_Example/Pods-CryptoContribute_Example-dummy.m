214142a6b087d6bf1ef5b805d9ee86c8abacecfb
Alexander Murphy <alex.murphy@ibotta.com>
2018-08-15 20:27:48 -0600

    adding example project

---
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Example/Pods-CryptoContribute_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoContribute_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoContribute_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-CryptoContribute_Tests/Pods-CryptoContribute_Tests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CryptoContribute_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CryptoContribute_TestsVersionString[];
+
